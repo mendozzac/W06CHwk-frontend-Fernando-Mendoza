9dd794bc897196f97a55db97610bdb6fee6c1190
Fernando Mendoza <87226298+mendozzac@users.noreply.github.com>
2021-11-06 20:01:09 +0100

    Remove useSelector and useDispatch from App to useRobots

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import { useEffect } from "react";
 import "./App.css";
+import Robot from "./components/Robot";
+import useRobots from "./hook/useRobots";
 
-function App() {
+function App(robots) {
+  const { loadRobots } = useRobots();
+
+  useEffect(() => {
+    loadRobots();
+  }, [loadRobots]);
+
   return (
     <>
-      <header>
-        <h1>Robotitos chachis</h1>
-      </header>
-      <main></main>
+      <div className="container">
+        <header>
+          <h1>Robotitos chachis</h1>
+        </header>
+        <main>
+          <div className="App">
+            {robots.map(({ robot }) => (
+              <li key="">
+                <Robot />
+              </li>
+            ))}
+          </div>
+        </main>
+      </div>
     </>
   );
 }
