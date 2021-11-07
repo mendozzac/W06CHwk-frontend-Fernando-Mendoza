43b92ca663e88e9e55eccdca2bbfac3f1476b9db
Fernando Mendoza <87226298+mendozzac@users.noreply.github.com>
2021-11-07 13:55:23 +0100

    Add a Robot component snapshot test

--- src/components/Robot/Robot.test.js
+++ src/components/Robot/Robot.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import ReactTestRenderer from "react-test-renderer";
 import Robot from "./Robot";
 
 describe("Given a Robot component", () => {
@@ -17,6 +18,21 @@ describe("Given a Robot component", () => {
       const newRobot = screen.getByText("Fredy");
 
       expect(newRobot).toBeInTheDocument();
+    });
+  });
+  describe("When it receives an object robot", () => {
+    test("Then it should render the robot", () => {
+      const robot = {
+        name: "Fredy",
+        image: "plastic",
+        features: {
+          speed: 5,
+          stamin: 6,
+        },
+      };
+
+      const newRobot = ReactTestRenderer.create(<Robot robot={robot} />);
+      expect(newRobot.toJSON()).toMatchSnapshot();
     });
   });
 });
