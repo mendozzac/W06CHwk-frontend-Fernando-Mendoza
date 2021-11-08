610f701b2a7901af7b0629a9e4c8aa33943745e2
Fernando Mendoza <87226298+mendozzac@users.noreply.github.com>
2021-11-08 21:17:44 +0100

    Add login and logout actionTypes

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -2,6 +2,8 @@ const actionTypes = {
   loadRobots: "LOAD_ROBOTS",
   loadRobotById: "LOAD_ROBOT_BY_ID",
   createRobot: "CREATE_ROBOT",
+  loginUser: "loginUser",
+  logoutUser: "logoutUser",
 };
 
 export default actionTypes;
