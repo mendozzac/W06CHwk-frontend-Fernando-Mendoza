2287567a3140dc2498bfb6088a5fb2341d7c08c5
Fernando Mendoza <87226298+mendozzac@users.noreply.github.com>
2021-11-08 22:05:54 +0100

    Add LoginForm structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import "./App.css";
+import LoginForm from "./components/LoginForm/LoginForm";
 import RobotForm from "./components/RobotForm/RobotForm";
 import RobotsList from "./components/RobotList/RobotsList";
 import useRobots from "./hook/useRobots";
@@ -18,6 +19,7 @@ function App() {
           <h1>Robotitos chachis</h1>
         </header>
         <main>
+          <LoginForm />
           <RobotForm />
           <RobotsList />
         </main>
