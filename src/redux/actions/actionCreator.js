909e4e18721a6a78bf752dba1cccb027ad25f4f0
Fernando Mendoza <87226298+mendozzac@users.noreply.github.com>
2021-11-08 21:18:34 +0100

    Add login and logout actionCreators

--- src/redux/actions/actionCreator.js
+++ src/redux/actions/actionCreator.js
@@ -14,3 +14,12 @@ export const createRobotAction = (robot) => ({
   type: actionTypes.createRobot,
   robot,
 });
+
+export const userLoginAction = (user) => ({
+  type: actionTypes.loginUser,
+  user,
+});
+
+export const userLogoutAction = () => ({
+  type: actionTypes.logoutUser,
+});
